fbcf094838544e83e25768f34cfe27444e36746c
Dhruv Patel <dhruv225@outlook.com>
2023-04-05 23:55:59 -0400

    test functions for length and hit

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -2,7 +2,14 @@ import ship from '../ship.js'
 
 
 test('ship is made', () => {
-    const obj = ship(4,0,false);
+    const obj = ship(4);
     expect(obj.length).toBe(4);
 });
+
+
+test('ship is hit', () => {
+    const obj = ship(4);
+    obj.hit();
+    expect(obj.hits).toBe(1);
+})
 
