bafed3b4f553db2cf7763155af5a6b7587276214
Dhruv Patel <dhruv225@outlook.com>
2023-04-05 23:13:40 -0400

    added babel for es6 modules

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,5 @@ module.exports = {
       ['@babel/preset-env', {targets: {node: 'current'}}],
       '@babel/preset-typescript',
     ],
-    "env": {
-        "test": {
-          "plugins": ["@babel/plugin-transform-modules-commonjs"]
-        }
-      }
+    
   };
