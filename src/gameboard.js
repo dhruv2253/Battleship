5b9f87277332b3a7aa09ee50ded6a8d11a414a98
Dhruv Patel <dhruv225@outlook.com>
2023-04-08 01:00:58 -0400

    fixed placeShip

--- src/gameboard.js
+++ src/gameboard.js
@@ -5,23 +5,23 @@ const cellContent = function() {
     let ship = null;
     let triedHit = false;
 
-    {return ship, triedHit}
+    return{ ship, triedHit}
 }
 
 
 const Gameboard = function() {
     // initialize board
     const initialize = function(size) {
-        let board = [];
+        let grid = [];
 
         // make board grid
         for (let i = 0; i < size; i++) {
-            board[i] = [];
+            grid[i] = [];
             for (let j = 0; j < size; j++) {
-                board[i][j] = cellContent();
+                grid[i][j] = cellContent();
             }
         }
-        return board;
+        return grid;
     }
 
     // board
@@ -31,31 +31,57 @@ const Gameboard = function() {
     let shipsOnBoard = [];
 
     // place ships onto board
-    const placeShip = function(x, y, newShip) {
+    const placeShip = function(x, y, newShip, vertical=false) {
 
         // coordinates of ship to place
         let startOfShip = [x,y];
-
-        if (x + newShip.length-1 >= 7) {
-            startOfShip = [7-newShip.length, y];
-        }
-        
-
-        let shipStartX = startOfShip[0];
-        let shipStartY = startOfShip[1];
-
-        // check overlapping 
-        for (let i = 0; i < newShip.length; i++) {
-            if (this.board[shipStartX + i][shipStartY].ship) {
-                return false;
+       
+        if (vertical === false) {
+            if (x + newShip.length-1 >= 7) {
+                startOfShip = [7-newShip.length, y];
             }
-        }
-        
-        
+            
+    
+            let shipStartX = startOfShip[0];
+            let shipStartY = startOfShip[1];
+    
+            // check overlapping 
+            for (let i = 0; i < newShip.length; i++) {
+                if (board[shipStartX + i][shipStartY].ship) {
+                    return false;
+                }
+            }
+            
+            
+    
+            shipsOnBoard.push(newShip);
+            for (let i = 0; i < newShip.length; i++) {
+                board[shipStartX + i][shipStartY].ship = newShip;
+            }
+            
+        } else {
+            if (y + newShip.length-1 >= 7) {
+                startOfShip = [x, 7-newShip.length];
+            }
 
-        shipsOnBoard.push(newShip);
-        for (let i = 0; i < newShip.length; i++) {
-            this.board[shipStartX + i][shipStartY] = newShip;
+            let shipStartX = startOfShip[0];
+            let shipStartY = startOfShip[1];
+
+            // check overlapping 
+            for (let i = 0; i < newShip.length; i++) {
+                if (board[shipStartY+i][shipStartX].ship) {
+                    return false;
+                }
+            }
+            
+            shipsOnBoard.push(newShip);
+
+            for (let i = 0; i < newShip.length; i++) {
+                board[shipStartY+i][shipStartX].ship = newShip;
+            }
+
+            
+
         }
         
 
@@ -71,7 +97,7 @@ const Gameboard = function() {
     // all ships sunk check
 
 
-    return {board, initialize, placeShip, cellContent, shipsOnBoard}
+    return {board, placeShip, shipsOnBoard}
 
 }
 
