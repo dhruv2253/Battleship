efe535e1e29b7f6aec71c81e8e6c78098ea45575
Dhruv Patel <dhruv225@outlook.com>
2023-04-09 23:45:15 -0400

    fixed issues and added comments

--- src/player.js
+++ src/player.js
@@ -3,25 +3,34 @@ import Gameboard from './gameboard.js'
 
 
 const Player = function() {
-    const playerBoard = new Gameboard();
+    // gameboard
+    const gameboard = new Gameboard();
+
+    // opponent
     const opp = null;
 
+    // commence player's attack on computer
     const playerAttack = function(x,y) {
         
+        // use gameboard's function receiveAttack
         return this.opp.gameboard.receiveAttack(x,y);
             
 
     }
 
+    // commence ai attack on player
     const aiAttack = function() {
 
-         x = Math.floor(Math.random() * 8);
-         y = Math.floor(Math.random() * 8);
-         return playerBoard.receiveAttack(x,y);
+        // randomize coordinates
+         x = Math.floor(Math.random() * 7);
+         y = Math.floor(Math.random() * 7);
+
+         //attack player board using gameboard's function receive attack
+         return this.opp.gameboard.receiveAttack(x,y);
     }
 
     
-    return {playerBoard, playerAttack, aiAttack, opp}
+    return {gameboard, playerAttack, aiAttack, opp}
     
 
 }
