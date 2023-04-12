82be4083a6651f3d7fd937a47216deb5374b1727
Dhruv Patel <dhruv225@outlook.com>
2023-04-12 02:03:56 -0400

    added comments and fixed createUserBoard

--- src/DOM.js
+++ src/DOM.js
@@ -11,25 +11,36 @@ const status = document.querySelector('.status');
 
 const createGame = function () {
 
+    // initialize players 
     const user = new Player();
     const ai = new Player();
+
+    // set opponents 
     user.opp = ai;
     ai.opp = user;
+
+    // keep track of whose turn it is
     let whoseTurn = user;
 
+    // ships to be placed on board
     const ships = [Ship[5], Ship[4], Ship[3]];
 
 
 
     
+    // Function that creates the user board and fills with squares containing datasets
     const createUserBoard = function () {
         for (let rows = 0; rows< user.gameboard.board.length; rows++) {
             for (let cols = 0; cols< user.gameboard.board[rows].length; cols++) {
+                // create div square
                 const gridSquare = document.createElement('div');
-                userBoard.classList.add('grid-square');
+                gridSquare.classList.add('grid-square');
+
+                // set data of grid squares to be equal to the (row, col)
                 gridSquare.dataset.x = cols;
                 gridSquare.dataset.y = rows;
-                userBoard.append(gridSquare);
+
+                userBoard.appendChild(gridSquare);
             }
         }
     }
